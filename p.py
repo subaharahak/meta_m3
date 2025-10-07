d7048ced581e59b595f59f76a7de3765cefc1e2a
subaharahak <subaharahak@gmail.com>
2025-10-07 08:58:07 +0530

    Update p.py

--- p.py
+++ p.py
@@ -292,12 +292,12 @@ def check_status(result):
         # Check if the extracted reason matches approved patterns
         for pattern in approved_patterns:
             if pattern in result:
-                return "APPROVED CC", "APPROVED", True
+                return "APPROVED CC", result, True
 
         # Check if the extracted reason matches CVV patterns
         for pattern in cvv_patterns:
             if pattern in reason_part:
-                return "APPROVED CC", "APPROVED", True
+                return "APPROVED CC", result, True
 
         # Return the extracted reason for declined cards
         return "DECLINED CC", reason_part, False
@@ -328,7 +328,7 @@ def check_status(result):
 
     for pattern in cvv_patterns:
         if pattern in result:
-            return "APPROVED CC", reason_part, True
+            return "APPROVED CC", result, True
 
     return "DECLINED CC", result, False
 
@@ -680,6 +680,7 @@ Bot By: 『@mhitzxg 帝 @pr0xy_xd』
     time.sleep(2)
 
 file.close()
+
 
 
 
